3f5041a2f3742413f2e7e9fe754f302cea14f0f6
DImiir <d.i.mironenko@yandex.ru>
2023-11-23 20:03:47 +0300

    That looks like second task in the lesson, but circles can be different colors.

---
+++ style_for.py
@@ -0,0 +1,24 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.do_btn = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.do_btn.setGeometry(QtCore.QRect(10, 10, 113, 32))
+        self.do_btn.setObjectName("do_btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Кружочки"))
+        self.do_btn.setText(_translate("MainWindow", "make a circle"))
